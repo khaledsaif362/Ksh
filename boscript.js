4f33fec6cf61a70626ad837d2ed246d0f5d4dcd6
khaledsaif362 <saif362@gmail.com>
2025-11-12 22:24:30 +0530

    boscript.js

--- boscript.js
+++ boscript.js
@@ -18,7 +18,7 @@ function generateContent() {
         const code = `${prefix}${num}`;
         switch (type) {
           case 'client':header=`RUPEE|CLT|${date}`;filename='client.txt';
-          rows.push(`${code}|TEST A|${code}|banagalore|123456||vsatish.iuy@gm.com|4TH CROSS||||group||||2|BSOOS8595P|NI|1234567890|560016|AXISBANK LTD.|CHAMARAJAPET|A||N||WEB|Y|E|||Y|07/07/1985||Q106|R16|||||7|||`);break;
+          rows.push(`${code}|TEST A|${code}|HO|123456||vuy@gm.com|4TH CROSS||||group||||2|ABCDE1234Q|NI|1234567890|560016|AXISBANK LTD.|CHAMARAJAPET|A||N||WEB|Y|E|||Y|07/07/1985|||R16|||||7|||`);break;
         
 
 case 'bank':header=`RUPEE|CBM|${date}`;filename='Bank_Update.txt';
